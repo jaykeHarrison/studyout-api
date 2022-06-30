e32aa4d82cf508b706dc98c1a89eb0e498706a52
garysum88 <102538356+garysum88@users.noreply.github.com>
2022-06-30 13:04:10 +0100

    Add PostLocation func

--- routes/location.go
+++ routes/location.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/jaykeHarrison/studyout-api/model"
+    "github.com/jaykeHarrison/studyout-api/models"
 	"github.com/jaykeHarrison/studyout-api/utils"
 )
 
@@ -14,3 +15,25 @@ func GetLocations(c *fiber.Ctx) error {
 
 	return c.Status(200).JSON(locations)
 }
+
+
+
+func PostLocation (c *fiber.Ctx) error {
+    var location models.Location
+
+    if err := c.BodyParser(&location); err != nil {
+        return c.Status(400).JSON(err.Error())
+    }
+
+    if err := model.AddLocation(&location); err != nil {
+        return c.Status(400).JSON(err.Error())
+    }
+
+    responseLocation := utils.CreateResponseLocation(location)
+    
+    return c.Status(200).JSON(responseLocation)
+
+}
+
+
+
