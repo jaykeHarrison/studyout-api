d4ebaf0ca0a8a162373822103252b01b143833a5
garysum88 <102538356+garysum88@users.noreply.github.com>
2022-06-30 13:02:46 +0100

    Add AddLocation func

--- model/location.go
+++ model/location.go
@@ -3,8 +3,14 @@ package model
 import (
 	"github.com/jaykeHarrison/studyout-api/database"
 	"github.com/jaykeHarrison/studyout-api/utils"
+	"github.com/jaykeHarrison/studyout-api/models"
 )
 
 func FetchLocations(locationSlice *[]utils.Location) {
 	database.Database.Db.Find(&locationSlice)
 }
+
+func AddLocation(location *models.Location) error {
+	return database.Database.Db.Create(&location).Error
+}
+
