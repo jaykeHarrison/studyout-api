b4aaa57a63e87cbd5fa66979d9154330cb28a8d6
garysum88 <102538356+garysum88@users.noreply.github.com>
2022-06-30 13:04:51 +0100

    Add Location struct, add CreateResponseLocation func

--- utils/utils.go
+++ utils/utils.go
@@ -3,17 +3,6 @@ import "time"
 
 import "github.com/jaykeHarrison/studyout-api/models"
 
-type Location struct {
-	LocationID   uint
-	LocationName string
-	Address      string
-	Postcode     string
-	Longitude    float32
-	Latitude     float32
-	Condition    string
-	ImgUrl       string
-	CreatedBy    uint
-}
 
 type User struct {
 	ID        uint `gorm:"primaryKey"`
@@ -41,4 +30,22 @@ type Review struct {
 	VisitDate string
 	StarRating int
 	ReviewBody string
+}
+
+type Location struct {
+    LocationID   uint  `gorm:"primaryKey"`
+    LocationName string  `gorm:"not null; default:null"`
+    Address      string  `gorm:"not null; default:null"`
+    Postcode     string  `gorm:"not null; default:null"`
+    Longitude    float32  `gorm:"not null; default:null"`
+    Latitude     float32  `gorm:"not null; default:null"`
+    Condition    string  `gorm:"not null; default:null"`
+    ImgUrl       string  `gorm:"not null; default:null"`
+    CreatedBy    uint
+    Users        User `gorm:"foreignKey:CreatedBy"`
+}
+
+
+func CreateResponseLocation(location models.Location) Location {
+    return Location {LocationID: location.LocationID, LocationName: location.LocationName, Address:location.Address, Postcode: location.Postcode, Longitude: location.Longitude, Latitude : location.Latitude, Condition: location.Condition, ImgUrl: location.ImgUrl , CreatedBy : location.CreatedBy}
 }
