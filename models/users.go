5173211bd856053130cbef7bc42cee14f7ea0a9f
jaykeHarrison <jayke@jaykeharrison.co.uk>
2022-06-30 11:49:50 +0100

    add not null constraint to users table

--- models/users.go
+++ models/users.go
@@ -1,9 +1,9 @@
 package models
 
 type Users struct {
-	ID        uint `gorm:"primaryKey"`
-	Username  string
-	FirstName string
-	LastName  string
-	Email     string
+	ID        uint   `gorm:"primaryKey"`
+	Username  string `gorm:"not null; default:null"`
+	FirstName string `gorm:"not null; default:null"`
+	LastName  string `gorm:"not null; default:null"`
+	Email     string `gorm:"not null; default:null"`
 }
