7f4ea3f931c7898eb1a6ca31ba450cf73270a9a8
garysum88 <102538356+garysum88@users.noreply.github.com>
2022-06-30 14:39:13 +0100

    Refactor Location struct - constraint of CreatedBy

--- utils/utils.go
+++ utils/utils.go
@@ -1,8 +1,10 @@
 package utils
-import "time"
 
-import "github.com/jaykeHarrison/studyout-api/models"
+import (
+	"time"
 
+	"github.com/jaykeHarrison/studyout-api/models"
+)
 
 type User struct {
 	ID        uint `gorm:"primaryKey"`
@@ -23,29 +25,28 @@ func CreateResponseUser(user models.Users) User {
 }
 
 type Review struct {
-	ReviewID     uint
-	UserRefer  uint
+	ReviewID      uint
+	UserRefer     uint
 	LocationRefer uint
-	CreatedAt	time.Time
-	VisitDate string
-	StarRating int
-	ReviewBody string
+	CreatedAt     time.Time
+	VisitDate     string
+	StarRating    int
+	ReviewBody    string
 }
 
 type Location struct {
-    LocationID   uint  `gorm:"primaryKey"`
-    LocationName string  `gorm:"not null; default:null"`
-    Address      string  `gorm:"not null; default:null"`
-    Postcode     string  `gorm:"not null; default:null"`
-    Longitude    float32  `gorm:"not null; default:null"`
-    Latitude     float32  `gorm:"not null; default:null"`
-    Condition    string  `gorm:"not null; default:null"`
-    ImgUrl       string  `gorm:"not null; default:null"`
-    CreatedBy    uint
-    Users        User `gorm:"foreignKey:CreatedBy"`
+	LocationID   uint    `gorm:"primaryKey"`
+	LocationName string  `gorm:"not null; default:null"`
+	Address      string  `gorm:"not null; default:null"`
+	Postcode     string  `gorm:"not null; default:null"`
+	Longitude    float32 `gorm:"not null; default:null"`
+	Latitude     float32 `gorm:"not null; default:null"`
+	Condition    string  `gorm:"not null; default:null"`
+	ImgUrl       string  `gorm:"not null; default:null"`
+	CreatedBy    uint    `gorm:"not null; default:null"`
+	Users        User    `gorm:"foreignKey:CreatedBy"`
 }
 
-
 func CreateResponseLocation(location models.Location) Location {
-    return Location {LocationID: location.LocationID, LocationName: location.LocationName, Address:location.Address, Postcode: location.Postcode, Longitude: location.Longitude, Latitude : location.Latitude, Condition: location.Condition, ImgUrl: location.ImgUrl , CreatedBy : location.CreatedBy}
+	return Location{LocationID: location.LocationID, LocationName: location.LocationName, Address: location.Address, Postcode: location.Postcode, Longitude: location.Longitude, Latitude: location.Latitude, Condition: location.Condition, ImgUrl: location.ImgUrl, CreatedBy: location.CreatedBy}
 }
