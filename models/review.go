1406cfb3a56a491ea1f5430bf6824a0f7a670faf
jaykeHarrison <jayke@jaykeharrison.co.uk>
2022-06-30 12:46:11 +0100

    add not null constraint

--- models/review.go
+++ models/review.go
@@ -3,14 +3,14 @@ package models
 import "time"
 
 type Review struct {
-	UserRefer  uint
-	LocationRefer uint
-	ReviewId	uint	`gorm:"primaryKey"`
-	CreatedAt	time.Time
-	UserId  Users `gorm:"foreignKey:UserRefer"`
-	LocationId	Location `gorm:"foreignKey:LocationRefer"`
-	VisitDate string
-	StarRating int
+	UserRefer     uint `gorm:"not null; default:null"`
+	LocationRefer uint `gorm:"not null; default:null"`
+	ReviewId      uint `gorm:"primaryKey"`
+	CreatedAt     time.Time
+	UserId        Users    `gorm:"foreignKey:UserRefer; not null; default:null"`
+	LocationId    Location `gorm:"foreignKey:LocationRefer; not null; default:null"`
+	VisitDate     string   `gorm:"not null; default:null"`
+	StarRating    int      `gorm:"not null; default:null"`
 	// Another ticket to handle the range of the star rating
-	ReviewBody string
+	ReviewBody string `gorm:"not null; default:null"`
 }
