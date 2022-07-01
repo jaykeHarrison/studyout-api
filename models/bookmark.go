4033a47a0ed473ccc18c09d2626f0afe11b7766f
nmathew222 <nikhilkmathew22@gmail.com>
2022-07-01 10:28:25 +0100

    Fix the bookmark table again

--- models/bookmark.go
+++ models/bookmark.go
@@ -1,7 +1,7 @@
 package models
 
 type Bookmark struct {
-	UserRefer  Users     `gorm:"foreignKey:UserRefer"`
+	UserRefer  Users     `gorm:"foreignKey:UserId"`
 	UserId     uint    `gorm: not null; default:null`
 	LocationId uint     `gorm: not null; default:null`
 	Location   Location `gorm:"foreignKey:LocationId"`
