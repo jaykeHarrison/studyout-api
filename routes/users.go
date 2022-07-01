3c29b349f83723e6412b43f2ed932da6c1ed97dd
jaykeHarrison <jayke@jaykeharrison.co.uk>
2022-07-01 12:12:08 +0100

    add GetUserById controller

--- routes/users.go
+++ routes/users.go
@@ -5,6 +5,8 @@ import (
 	"github.com/jaykeHarrison/studyout-api/model"
 	"github.com/jaykeHarrison/studyout-api/models"
 	"github.com/jaykeHarrison/studyout-api/utils"
+	"reflect"
+	"strconv"
 )
 
 func PostUser(c *fiber.Ctx) error {
@@ -19,6 +21,30 @@ func PostUser(c *fiber.Ctx) error {
 	}
 
 	responseUser := utils.CreateResponseUser(newUser)
+
+	return c.Status(200).JSON(responseUser)
+}
+
+func GetUserById(c *fiber.Ctx) error {
+	var fetchedUser models.Users
+
+	userIdString := c.Params("user_id")
+
+	userIdInt, _ := strconv.ParseInt(userIdString, 10, 0)
+
+	if reflect.TypeOf(userIdInt).String() != "int64" {
+		return c.Status(400).JSON("user_id must be an integer")
+	}
+
+	if userIdInt < 1 {
+		return c.Status(400).JSON("invalid user_id")
+	}
+
+	if err := model.FetchUserById(&fetchedUser, userIdInt); err != nil {
+		return c.Status(404).JSON(err.Error())
+	}
+
+	responseUser := utils.CreateResponseUser(fetchedUser)
 
 	return c.Status(200).JSON(responseUser)
 }
