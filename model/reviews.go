8ec55690031739cffe5f471b0250dbbbb86176b2
jaykeHarrison <jayke@jaykeharrison.co.uk>
2022-07-01 11:18:51 +0100

    add POST Review model

--- model/reviews.go
+++ model/reviews.go
@@ -2,9 +2,17 @@ package model
 
 import (
 	"github.com/jaykeHarrison/studyout-api/database"
+	"github.com/jaykeHarrison/studyout-api/models"
 	"github.com/jaykeHarrison/studyout-api/utils"
+	"time"
 )
 
 func FetchReviews(reviewSlice *[]utils.Review, location_refer string) {
 	database.Database.Db.Where("location_refer = ?", location_refer).Find(&reviewSlice)
+}
+
+func AddReview(review *models.Review) error {
+	database.Database.Db.Create(review)
+
+	return database.Database.Db.Model(&review).Update("CreatedAt", time.Now()).Error
 }
