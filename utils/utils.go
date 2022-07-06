77b30072f41e09a12975ea3f9f7f713dcb159900
jaykeHarrison <jayke@jaykeharrison.co.uk>
2022-07-06 12:20:10 +0100

    add response features utils update response location

--- utils/utils.go
+++ utils/utils.go
@@ -69,10 +69,68 @@ type LocationResponse struct {
 	Condition    string
 	ImgUrl       string
 	CreatedBy    string
+	Features     LocationFeature
 }
 
-func CreateResponseLocation(location models.Location) LocationResponse {
-	return LocationResponse{LocationID: location.LocationID, LocationName: location.LocationName, Address: location.Address, Postcode: location.Postcode, Longitude: location.Longitude, Latitude: location.Latitude, Condition: location.Condition, ImgUrl: location.ImgUrl, CreatedBy: location.Users.Username}
+type LocationFeature struct {
+	WiFi                 bool `gorm: type:bool; default:false`
+	WaterFountain        bool `gorm: type:bool; default:false`
+	PowerSockets         bool `gorm: type:bool; default:false`
+	Noisy                bool `gorm: type:bool; default:false`
+	Food                 bool `gorm: type:bool; default:false`
+	Beverages            bool `gorm: type:bool; default:false`
+	Toilets              bool `gorm: type:bool; default:false`
+	WheelchairAccess     bool `gorm: type:bool; default:false`
+	DiasabledToilets     bool `gorm: type:bool; default:false`
+	GenderNeutralToilets bool `gorm: type:bool; default:false`
+	FreeParking          bool `gorm: type:bool; default:false`
+	PaidParking          bool `gorm: type:bool; default:false`
+	PetFriendly          bool `gorm: type:bool; default:false`
+	Vegetarian           bool `gorm: type:bool; default:false`
+	Vegan                bool `gorm: type:bool; default:false`
+	OutdoorSeating       bool `gorm: type:bool; default:false`
+	PhoneSignal          bool `gorm: type:bool; default:false`
+	NaturalLight         bool `gorm: type:bool; default:false`
+}
+
+func CreateResponseLocationFeature(locationFeature models.LocationFeature) LocationFeature {
+	return LocationFeature{
+		WiFi:                 locationFeature.WiFi,
+		WaterFountain:        locationFeature.WaterFountain,
+		PowerSockets:         locationFeature.PowerSockets,
+		Noisy:                locationFeature.Noisy,
+		Food:                 locationFeature.Food,
+		Beverages:            locationFeature.Beverages,
+		Toilets:              locationFeature.Toilets,
+		WheelchairAccess:     locationFeature.WheelchairAccess,
+		DiasabledToilets:     locationFeature.DiasabledToilets,
+		GenderNeutralToilets: locationFeature.GenderNeutralToilets,
+		FreeParking:          locationFeature.FreeParking,
+		PaidParking:          locationFeature.PaidParking,
+		PetFriendly:          locationFeature.PetFriendly,
+		Vegetarian:           locationFeature.Vegetarian,
+		Vegan:                locationFeature.Vegan,
+		OutdoorSeating:       locationFeature.OutdoorSeating,
+		PhoneSignal:          locationFeature.PhoneSignal,
+		NaturalLight:         locationFeature.NaturalLight,
+	}
+}
+
+func CreateResponseLocation(location models.Location, locationFeature models.LocationFeature) LocationResponse {
+	responseLocationFeature := CreateResponseLocationFeature(locationFeature)
+
+	return LocationResponse{
+		LocationID:   location.LocationID,
+		LocationName: location.LocationName,
+		Address:      location.Address,
+		Postcode:     location.Postcode,
+		Longitude:    location.Longitude,
+		Latitude:     location.Latitude,
+		Condition:    location.Condition,
+		ImgUrl:       location.ImgUrl,
+		CreatedBy:    location.Users.Username,
+		Features:     responseLocationFeature,
+	}
 }
 
 type BookmarkResponse struct {
